271c436c81de1c9f6abe2e9d39c405c406b9eea4
Dariusz Okoński <darek200180@gmail.com>
2024-07-04 18:04:45 +0200

    add tests form password

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           />
         </div>
 
-        <div>
+        <div className='mb-3'>
           <label htmlFor='password' className='form-label'>
             Password
           </label>
@@ -26,10 +26,11 @@ function App() {
             id='password'
             name='password'
             className='form-control'
+            data-testid='password'
           />
         </div>
 
-        <div>
+        <div className='mb-3'>
           <label htmlFor='confirm-password' className='form-label'>
             Confirm password
           </label>
@@ -38,6 +39,7 @@ function App() {
             id='confirm-password'
             name='confirm-password'
             className='form-control'
+            data-testid='confirm-password'
           />
         </div>
       </form>
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 test('inputs should be initially empty', () => {
   render(<App />);
@@ -10,4 +11,25 @@ test('inputs should be initially empty', () => {
   expect(emailInputElement.value).toBe('');
   expect(passwordInputElement.value).toBe('');
   expect(confirmPasswordInputElement.value).toBe('');
+});
+
+test('should be able to type an email', () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole('textbox', { name: /email/i });
+  userEvent.type(emailInputElement, 'selena@gmail.com');
+  expect(emailInputElement.value).toBe('selena@gmail.com');
+});
+
+test('should be able to type password', () => {
+  render(<App />);
+  const passwordInputElement = screen.getByTestId('password');
+  userEvent.type(passwordInputElement, 'password1');
+  expect(passwordInputElement.value).toBe('password1');
+});
+
+test('should be able to type confirm password', () => {
+  render(<App />);
+  const confirmPasswordInputElement = screen.getByTestId('confirm-password');
+  userEvent.type(confirmPasswordInputElement, 'secret123');
+  expect(confirmPasswordInputElement.value).toBe('secret123');
 });
